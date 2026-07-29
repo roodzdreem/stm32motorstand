d9da387dae8476ab211d6c1135c61a47af62273f
Dmitrii <dima.babchik2408@gmail.com>
2026-07-30 01:38:57 +0300

    feat: Remove hal and add ll

--- src/main.h
+++ src/main.h
@@ -27,7 +27,6 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f7xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
